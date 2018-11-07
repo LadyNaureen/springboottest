c841ec16e2e3db8911b1a357be9ab47a1df7b92c
hammadnaeem <43746340+hammadnaeem@users.noreply.github.com>
2018-11-07 18:20:56 +0500

    Create SomeUnitTest.java

---
+++ src/test/java/hello/SomeUnitTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SomeUnitTest {
+    @Test
+    public void init() {
+        assertEquals(1, 1);
+    }
+}
